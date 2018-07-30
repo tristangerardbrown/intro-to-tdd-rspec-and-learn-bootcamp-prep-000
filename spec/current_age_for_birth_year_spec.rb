b1176332f567484c91969e92a6d8e8a5de8aa24c
Tristan Brown <tgbrown@stanford.edu>
2018-07-30 21:12:35 +0000

    automatically backed up by learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../current_age_for_birth_year.rb'
 
-def current_age_for_birth_year.
+def current_age_for_birth_year (birth_year)
 end
 
 describe "current_age_for_birth_year method" do
